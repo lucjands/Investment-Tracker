f0d630d62cf0607c1e85b95ac6fbf18836a57988
lucjan <lucjan-_-@live.be>
2021-11-27 17:05:42 +0100

    Created an initial model for cryptocurrencies added to the application.

---
+++ Investment Tracker/Model/Cryptocurrency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investment_Tracker.Model
+{
+    class Cryptocurrency
+    {
+        public int AssetID { get; set; }
+
+        public int CryptoID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Ticker { get; set; }
+    }
+}
